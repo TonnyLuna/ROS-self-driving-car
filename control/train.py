313c5c304d803fe1ce4e9d08c862ab0f6f992e80
TonnyLuna <96.antonioluna@gmail.com>
2019-09-23 01:11:12 -0500

    update

--- control/train.py
+++ control/train.py
@@ -146,7 +146,7 @@ def main():
     """
     parser = argparse.ArgumentParser(description='Behavioral Cloning Training Program')
     parser.add_argument('-p', help='Optimizer',             dest='opt',               type=str,   default='Adam')
-    parser.add_argument('-d', help='data directory',        dest='data_dir',          type=str,   default='/home/tonny/catvehicle_ws/control/data')
+    parser.add_argument('-d', help='data directory',        dest='data_dir',          type=str,   default='../database')
     parser.add_argument('-t', help='test size fraction',    dest='test_size',         type=float, default=0.2)
     parser.add_argument('-k', help='drop out probability',  dest='keep_prob',         type=float, default=0.5)
     parser.add_argument('-n', help='number of epochs',      dest='nb_epoch',          type=int,   default=15)
